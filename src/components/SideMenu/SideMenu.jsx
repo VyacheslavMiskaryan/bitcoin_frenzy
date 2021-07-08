65c8598b491d140f8778057126dbcb6fc3387642
VyacheslavMiskaryan <v.miskaryan@dunice.net>
2021-07-08 08:48:42 +0300

    fixed structure

--- src/components/SideMenu/SideMenu.jsx
+++ src/components/SideMenu/SideMenu.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
 import MenuLink from '../MenuLink';
-import menuLinks from './utils/constants';
+import menuLinks from './constants';
 import './SideMenuStyles.sass';
 
 const SideMenu = () => {
